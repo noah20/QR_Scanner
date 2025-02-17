25609e6ed81af28c138f48ddbc8728dbaf5c3a16
Noah.Mohamed <noah.mohamed@fawry.com>
2025-02-17 04:57:11 +0200

    handle camera permission

--- app/src/main/java/com/dev/qrscanner/main/app/HomeActivity.java
+++ app/src/main/java/com/dev/qrscanner/main/app/HomeActivity.java
@@ -1,10 +1,15 @@
 package com.dev.qrscanner.main.app;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
+
 import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -12,6 +17,7 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
+
 import com.dev.qrscanner.R;
 import com.dev.qrscanner.databinding.ActivityMainBinding;
 import com.dev.qrscanner.main.data.model.ScannerDataDialogUiConfig;
@@ -24,32 +30,56 @@ import com.journeyapps.barcodescanner.ScanOptions;
 import dagger.hilt.android.AndroidEntryPoint;
 
 @AndroidEntryPoint
-public class MainActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
 
     private HomeViewModel mViewModel;
 
-    private final ActivityResultLauncher<ScanOptions> barcodeLauncher = registerForActivityResult(new ScanContract(),
-            this::handleResult);
+    private final ActivityResultLauncher<ScanOptions> barcodeLauncher =
+            registerForActivityResult(new ScanContract(), this::handleResult);
+
+    private final ActivityResultLauncher<String> requestCameraPermission =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), this::handleCameraPermission);
 
 
     private void handleResult(ScanIntentResult result) {
         if (result.getContents() == null) {
-            ScannerDataDialogUiConfig config = new  ScannerDataDialogUiConfig("Canceled", "Scan canceled" , "got it");
+            ScannerDataDialogUiConfig config = new ScannerDataDialogUiConfig(getString(R.string.canceled), getString(R.string.scan_canceled), getString(R.string.got_it));
             ScannerDataDialog d = ScannerDataDialog.newInstance(config, null);
-            d.show(getSupportFragmentManager() , "canceled");
+            d.show(getSupportFragmentManager(), "canceled");
         } else {
-            saveCode(result.getContents());
+            mViewModel.insertCode(result.getContents());
         }
     }
 
-    private void saveCode(String content) {
-        mViewModel.insertCode(content);
+    public void onScanClick(View view) {
+
+        if (permissionGranted()) {
+            barcodeLauncher.launch(new ScanOptions());
+        } else {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+                ScannerDataDialogUiConfig config = new ScannerDataDialogUiConfig(getString(R.string.permission_required), getString(R.string.camera_permission_denied), getString(R.string.grant));
+                ScannerDataDialog d = ScannerDataDialog.newInstance(config, () -> {
+                    requestCameraPermission.launch(Manifest.permission.CAMERA);
+                });
+                d.show(getSupportFragmentManager(), "canceled");
+
+            } else {
+                requestCameraPermission.launch(Manifest.permission.CAMERA);
+            }
+        }
     }
 
-    public void onScanClick(View view) {
-        barcodeLauncher.launch(new ScanOptions());
+    private boolean permissionGranted() {
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void handleCameraPermission(Boolean o) {
+        if (o) {
+            barcodeLauncher.launch(new ScanOptions());
+        }
     }
 
     @Override
@@ -64,6 +94,7 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host);
         NavigationUI.setupWithNavController(binding.navView, navController);
 
--- app/src/main/java/com/dev/qrscanner/main/ui/home/HomeFragment.java
+++ app/src/main/java/com/dev/qrscanner/main/ui/home/HomeFragment.java
@@ -1,16 +1,13 @@
 package com.dev.qrscanner.main.ui.home;
 
 import androidx.activity.result.ActivityResultLauncher;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
@@ -19,12 +16,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.dev.qrscanner.R;
 import com.dev.qrscanner.databinding.FragmentHomeBinding;
 import com.dev.qrscanner.main.app.BaseFragment;
-import com.dev.qrscanner.main.app.MainActivity;
+import com.dev.qrscanner.main.app.HomeActivity;
 import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanIntentResult;
 import com.journeyapps.barcodescanner.ScanOptions;
@@ -80,7 +76,7 @@ public class HomeFragment extends BaseFragment {
                 R.id.navigation_history, R.id.navigation_favorite)
                 .build();
         NavController navController =  NavHostFragment.findNavController(this);
-        NavigationUI.setupActionBarWithNavController((MainActivity) requireActivity(), navController, appBarConfiguration);
+        NavigationUI.setupActionBarWithNavController((HomeActivity) requireActivity(), navController, appBarConfiguration);
         NavigationUI.setupWithNavController(mBinding.navView, navController);
 
     }
